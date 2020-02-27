0c06901e89295ab248506d62691a3cc2fbc499b9
Alim Minadzhiev <alim.minadzhiev@thomsonreuters.com>
2020-02-27 19:33:23 +0200

    Added armors

---
+++ src/main/java/org/aminadzh/swingy/model/items/armor/LetherArmor.java
@@ -0,0 +1,11 @@
+package org.aminadzh.swingy.model.items.armor;
+
+import org.aminadzh.swingy.model.items.Armor;
+
+public class LetherArmor extends Armor {
+
+    public LetherArmor() {
+        super("Lether Armor", 1, "assets/items/lether_armor.png");
+    }
+
+}
---
+++ src/main/java/org/aminadzh/swingy/model/items/armor/SteelArmor.java
@@ -0,0 +1,11 @@
+package org.aminadzh.swingy.model.items.armor;
+
+import org.aminadzh.swingy.model.items.Armor;
+
+public class SteelArmor extends Armor {
+
+    public SteelArmor() {
+        super("Steel Armor", 2, "assets/items/steel_armor.png");
+    }
+
+}
